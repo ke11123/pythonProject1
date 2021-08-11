ffe1e91be1f236236052117444407274d9e6e9cf
pengke <1354313932@qq.com>
2021-08-12 00:06:24 +0800

    Initial commit test

---
+++ test1.py
@@ -0,0 +1,7 @@
+#from decimal import decimal
+n1=1.1
+n2=2.2
+print(n1+n2)
+a='hello'
+b=int(a)
+print(b)
---
+++ test2.py
@@ -0,0 +1,8 @@
+a=1
+a=+10
+a+=10
+print(a)
+a,b=10,20
+print('交换前a,b的值为:',a,b)
+a,b=b,a
+print('交换后a,b的值为：',a,b)
